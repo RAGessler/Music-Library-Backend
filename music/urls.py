e3d161b38687aa410783030494b2bda74b5da4e4
Robert Gessler <ragessler@Roberts-MBP.kc.rr.com>
2022-05-06 09:25:08 -0500

    created API view for post and get all

--- music/urls.py
+++ music/urls.py
@@ -1,7 +1,7 @@
-# from django.urls import path
-# from . import views
+from django.urls import path
+from . import views
 
-# urlpatterns = [
-#     path('', views.music_library),
-#     path('<int:pk>/', views.music_detail)
-# ]
+urlpatterns = [
+    path('', views.music_library),
+    path('<int:pk>/', views.music_detail)
+]
--- music/views.py
+++ music/views.py
@@ -1,5 +1,19 @@
-from django.shortcuts import render
-
+from django.shortcuts import get_object_or_404
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .serializer import MusicSerializer
+from .models import Music
 # Create your views here.
 
-# @api_view(['GET','POST'])
+@api_view(['GET','POST'])
+def music_library(request):
+    if request.method == 'GET':
+        music = Music.objects.all()
+        serializer = MusicSerializer(music, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    elif request.method == 'POST':
+        serializer = MusicSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
