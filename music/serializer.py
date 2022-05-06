3691571475efec6e8c8f846691d22e702cfcfb0f
Robert Gessler <ragessler@Roberts-MBP.kc.rr.com>
2022-05-06 08:50:17 -0500

    created superuser + fixed some typos + verified site is working.

---
+++ music/serializer.py
@@ -0,0 +1,9 @@
+from dataclasses import fields
+from pyexpat import model
+from rest_framework import serializers
+from .models import Music
+
+class MusicSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Music
+        fields = ['id','title','album','release_date','genre','likes']
