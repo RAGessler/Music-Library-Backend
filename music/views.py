0b42125d9a22700fc2b9a9bf88d7001adeb61f52
Robert Gessler <ragessler@Roberts-MBP.attlocal.net>
2022-05-06 22:27:23 -0500

    cleaned up some extra spacing

--- music/views.py
+++ music/views.py
@@ -42,5 +42,3 @@ def music_detail(request, pk):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data, status=status.HTTP_200_OK)
-        
-
